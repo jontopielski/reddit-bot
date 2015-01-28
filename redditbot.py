17f5ac90a8e1aaf9ebfdcb14e85ab0bf61e3a0be
Jon Topielski <polkorame@gmail.com>
2015-01-27 23:33:22 -0800

    removed running function and updated comment reader

--- redditbot.py
+++ redditbot.py
@@ -3,38 +3,34 @@ import time
 
 r = praw.Reddit(user_agent = "fashion_info_finder bot v1.0 by /u/fashion_info_finder")
 running = True
-retrieve_limit = 25
+retrieve_limit = 5
 counter = 0
 questions = []
 answers = []
 
-# primary function
-def bot_run():
-	mfa_url = 'http://www.reddit.com/r/malefashionadvice'
-	content = r.get_content(mfa_url, limit = retrieve_limit)
-
-	for submission in content:
-		if '?' in submission.title and submission.id not in questions:
-			print(submission.title)
-			questions.append(submission.id)
-			'''
-			comments = submission.get_comments
-			top_comment = comments.get_top(comments)
-			print(top_comment)
-			print('\n')
-			'''
+mfa_url = 'http://www.reddit.com/r/malefashionadvice'
+content = r.get_content(mfa_url, limit = retrieve_limit)
+subreddit = r.get_subreddit("malefashionadvice")
 
 # login preload
 r.login("fashion_info_finder", "topielski")
 print('Logging in...')
 
-# 'main' function
-while (running):
-	bot_run()
+# primary function
+while (True):
+	for submission in content:
+		if '?' in submission.title and submission.id not in questions:
+			print('Question:')
+			print(submission.title)
+			questions.append(submission.id)
+			
+			comments = subreddit.get_comments(limit=retrieve_limit)
+			for comment in comments:
+				comment_text = comment.body.lower()
+				print('Answer:')
+				print(comment_text)
+			print
+	
 	time.sleep(5)
-	counter += 1
-	print('Bot run ', counter, 'times...')
-	print('Terminating after 5 executions...')
+	print('Reached end...')
 
-	if (counter == 5):
-		running = False
