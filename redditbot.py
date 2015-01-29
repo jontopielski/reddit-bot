a220139ea8d86914887be2030108a238fa8809ee
Jon Topielski <polkorame@gmail.com>
2015-01-28 22:44:42 -0800

    Added top comment finder from submission

--- redditbot.py
+++ redditbot.py
@@ -1,13 +1,19 @@
 import praw
 import time
 
+# reddit wrapper usage and defined user_agent
 r = praw.Reddit(user_agent = "fashion_info_finder bot v1.0 by /u/fashion_info_finder")
-running = True
-retrieve_limit = 5
-counter = 0
+# make 5 requests per call
+retrieve_limit = 25
+# holds the questions and answers in a list
+questions_id_cache = []
 questions = []
 answers = []
 
+# for running once and exiting
+running = True
+
+# url, content extracted from url, and desired subreddit
 mfa_url = 'http://www.reddit.com/r/malefashionadvice'
 content = r.get_content(mfa_url, limit = retrieve_limit)
 subreddit = r.get_subreddit("malefashionadvice")
@@ -17,20 +23,38 @@ r.login("fashion_info_finder", "topielski")
 print('Logging in...')
 
 # primary function
-while (True):
+while (running):
+
+	# loop through all submissions
 	for submission in content:
-		if '?' in submission.title and submission.id not in questions:
-			print('Question:')
-			print(submission.title)
-			questions.append(submission.id)
-			
-			comments = subreddit.get_comments(limit=retrieve_limit)
+
+		# if it is a question, store the title and score
+		if '?' in submission.title and submission.id not in questions_id_cache:
+			print '\nQuestion: ', submission.title
+			print '\nScore: ', submission.score
+			questions_id_cache.append(submission.id)
+			questions.append(submission.title)
+
+			# grab the comments from that submission
+			comments = submission.comments
+			top_score = 0
+			top_comment = ''
+
+			# loop through every comment, finding the top score
 			for comment in comments:
 				comment_text = comment.body.lower()
-				print('Answer:')
-				print(comment_text)
-			print
-	
-	time.sleep(5)
-	print('Reached end...')
+				comment_score = comment.score
+				if (comment_score > top_score):
+					top_comment = comment_text
+					top_score = comment.score
+					
+			answers.append(top_comment)
 
+			print '\n================================='
+
+	running = False
+
+	for counter in range(0, len(questions)):
+		print 'Question ', counter+1, ': ', questions[counter], '\n'
+		print 'Answer ', counter+1, ': ', answers[counter], '\n'
+		print '\n================================='
